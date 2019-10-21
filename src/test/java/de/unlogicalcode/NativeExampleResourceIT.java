84dfbf580195035a7ee7abe6faaf866e8e354a0e
unlogicalcode <jearsmail99@gmail.com>
2019-10-21 19:02:25 +0200

    inital commit: generated quarkus template from code.quarkus.io, without any plugins

---
+++ src/test/java/de/unlogicalcode/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package de.unlogicalcode;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
